37d04c8973cb9a48b39fdd8f4c883c98fe4a9bd3
vutr <me@vutr.io>
2019-10-11 16:06:05 +0700

    Tournamentor app Init

---
+++ tournamentor/clubs/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Club(models.Model):
+    name = models.TextField()
+    logo = models.CharField(max_length=300)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
