e1156c6bc2fb3a95483a8860864f73b866eedd33
vutr <me@vutr.io>
2019-10-11 16:15:14 +0700

    Add club model & registered

--- tournamentor/clubs/views.py
+++ tournamentor/clubs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Club
 
-# Create your views here.
+
+class ClubListView(ListView):
+    model = Club
+    paginate_by = 100
--- tournamentor/tournamentor/urls.py
+++ tournamentor/tournamentor/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from clubs.views import ClubListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clubs', ClubListView.as_view(), name='club-list'),
 ]
